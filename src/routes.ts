5b78c9d31cfb6e5c21a5474a315459a2be934fc7
JoaoLDF <joaofilhodias3@gmail.com>
2021-06-23 00:31:15 -0300

    Criada a rota para criação de tags, fazendo uso de middleware de verificação do status de admin do usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
+import { ensureAdmin } from './middlewares/ensureAdmin';
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post('/users/create', createUserController.handle)
+router.post('/tags/create', ensureAdmin, createTagController.handle)
 
 
 export { router };
